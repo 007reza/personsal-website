03336b4af93452b528531c2764f72dc2bf001439
reza <r.mohammadkhan@gmail.com>
2018-11-30 12:34:49 +0330

    navbar and searchbar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-
+import Navbar from './components/navbar';
 
 class App extends Component {
-  render() {
-    return (
-    <React.Fragment>
-      
-    </React.Fragment>
-    );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Navbar />
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
